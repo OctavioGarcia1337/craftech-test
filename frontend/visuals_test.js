73189e0791a1945c02229a9917eecbcb08129bb5
Octavio Garcia <80827553+OctavioGarcia1337@users.noreply.github.com>
2024-02-12 20:23:34 -0300

    Pls work n99192391293

--- frontend/visuals_test.js
+++ frontend/visuals_test.js
@@ -5,6 +5,36 @@ const assert = require('assert');
 //const URL = 'http://localhost:3000';
 const URL = 'https://gc-ingsw3-integrador-frontend-g5wdfg2tqq-uc.a.run.app'
 
+
+Scenario('Testing Upload - Error', async ({ I }) => {
+    I.amOnPage(URL);
+    I.click('button.sc-kFCroH');
+
+    I.waitForVisible('div.sc-guDLey.dwEfBc', 5);
+    
+    I.see('Desea añadir un nuevo audio de los viernes?');
+    const enlaceYouTube = 'https://www.youtube.com/shorts/TEST-ERROR';
+    I.fillField('input.sc-brSamD.bAiYgC', enlaceYouTube);
+
+    I.wait(1);
+
+    const colorAntes = await I.grabAttributeFrom('button.sc-kFCroH.sc-ktwOfi.kDXnIh.lcJajR', 'color');
+
+    I.click('button.sc-kFCroH.sc-ktwOfi.kDXnIh.lcJajR');
+
+    I.wait(1);
+
+    const colorDespues = await I.grabAttributeFrom('button.sc-kFCroH.sc-ktwOfi.QRzSJ.lcJajR', 'color');
+
+    if (colorAntes === 'default' && colorDespues === 'error') {
+        console.log('El audio no se añadio de manera exitosa. Lo cual es correcto para la prueba');
+    } else {
+        assert.fail(`Ocurrio un error inesperado`);
+        //assert.fail(`Ocurrio un error y se pudo añadir el audio. Antes:${colorAntes} ---- Desp:${colorDespues}`);
+    }
+
+});
+
 Scenario('Testing Upload - Success', async ({ I }) => {
     I.wait(10);
     I.amOnPage(URL);
@@ -35,31 +65,8 @@ Scenario('Testing Upload - Success', async ({ I }) => {
 
 });
 
-Scenario('Testing Upload - Error', async ({ I }) => {
+Scenario('Testing Some Text', async ({ I }) => {
     I.amOnPage(URL);
-    I.click('button.sc-kFCroH');
-
-    I.waitForVisible('div.sc-guDLey.dwEfBc', 5);
-    
-    I.see('Desea añadir un nuevo audio de los viernes?');
-    const enlaceYouTube = 'https://www.youtube.com/shorts/TEST-ERROR';
-    I.fillField('input.sc-brSamD.bAiYgC', enlaceYouTube);
-
-    I.wait(1);
-
-    const colorAntes = await I.grabAttributeFrom('button.sc-kFCroH.sc-ktwOfi.kDXnIh.lcJajR', 'color');
-
-    I.click('button.sc-kFCroH.sc-ktwOfi.kDXnIh.lcJajR');
-
-    I.wait(1);
-
-    const colorDespues = await I.grabAttributeFrom('button.sc-kFCroH.sc-ktwOfi.QRzSJ.lcJajR', 'color');
-
-    if (colorAntes === 'default' && colorDespues === 'error') {
-        console.log('El audio no se añadio de manera exitosa. Lo cual es correcto para la prueba');
-    } else {
-        assert.fail(`Ocurrio un error inesperado`);
-        //assert.fail(`Ocurrio un error y se pudo añadir el audio. Antes:${colorAntes} ---- Desp:${colorDespues}`);
-    }
-
+    I.wait(5);
+    I.see('EL AUDIO DE LOS VIERNES!')
 });
