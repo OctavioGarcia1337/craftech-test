bceb7dabe06ae5521480d2683bfc1b39ccdc1564
Octavio Garcia <80827553+OctavioGarcia1337@users.noreply.github.com>
2024-06-04 03:23:19 -0300

    well, lets try this

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,4 @@
 //export const DB_HOST = "http://localhost:8080" + '/api/audio';
 
 // eslint-disable-next-line
-export const DB_HOST = "gc-ct-backend-apoeka5nza-uc.a.run.app/audio/api/v1/audio/";
+export const DB_HOST = "gc-ct-backend-apoeka5nza-uc.a.run.app/audio/api/v1/audio";
